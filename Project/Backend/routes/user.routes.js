86b190cd4385be38208f0fd6b1837a52ab1eabc2
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2023-09-23 21:02:23 +0300

    more user routes have been added

--- Project/Backend/routes/user.routes.js
+++ Project/Backend/routes/user.routes.js
@@ -1,8 +1,32 @@
 const User = require("../App/controllers/user.controllers");
+const { userAuth, adminAuth } = require("../App/middleware/user.middle");
+const upload = require("../App/middleware/user.upload");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello from users");
-});
+// actions with no authentications
+router.post("/signup", User.signUp);
+router.post("/login", User.logIn);
+
+// actions for user authentications
+router.post("/logout", userAuth, User.logOut);
+router.post("/showprofile", userAuth, User.showProfile);
+router.post("/editprofile", userAuth, User.editProfile);
+router.delete("/deleteAcc", userAuth, User.deleteAcc);
+router.post("/uploadImage", userAuth, upload.single("Image"), User.uploadImage);
+router.post("/changepassword", userAuth, User.changePassword);
+
+// admin authentications
+router.post("/changestatus/:id", userAuth, adminAuth, User.changeStatus);
+router.delete(
+  "/deleteUser/:id",
+  userAuth,
+  adminAuth,
+  User.deleteUserAccFromAdmin
+);
+router.post("/show/:id", userAuth, adminAuth, User.showSingleUser);
+router.post("/showall", userAuth, adminAuth, User.showAllUsers);
+router.post("/showactive", userAuth, adminAuth, User.showActiveUsers);
+router.post("/showinactive", userAuth, adminAuth, User.showInactiveUsers);
+router.post("/addAdmin", userAuth, adminAuth, User.addAdmin);
 
 module.exports = router;
