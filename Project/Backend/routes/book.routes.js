158ef03249eb5bf68f58b96e12bf70363221dc72
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2023-09-23 21:01:46 +0300

    more book routes have been added

--- Project/Backend/routes/book.routes.js
+++ Project/Backend/routes/book.routes.js
@@ -1,8 +1,13 @@
 const Book = require("../App/controllers/book.controller");
+const { userAuth } = require("../App/middleware/user.middle");
 const router = require("express").Router();
 
-router.get("/books", (req, res) => {
-  res.send("Hello from books");
-});
+router.post("/searchbook", userAuth, Book.searchBook);
+router.post("/addbook/:id", userAuth, Book.addBook);
+router.delete("/removebook/:id", userAuth, Book.removeBook);
+router.post("/addbookwish/:id", userAuth, Book.addBookWish);
+router.delete("/removebookwish/:id", userAuth, Book.removeBookWish);
+router.post("/showallbooks", userAuth, Book.showAllBooks);
+router.post("/showallwishBooks", userAuth, Book.showWishBooks);
 
 module.exports = router;
